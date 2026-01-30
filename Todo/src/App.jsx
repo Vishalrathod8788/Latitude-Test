d6e8ff9d9cfc724e13b400de2dbc07d1cca28f49
Vishal Rathod <vishalrathod1562004@gmail.com>
2026-01-30 14:18:15 +0530

    Developed Todo Task Management App

--- Todo/src/App.jsx
+++ Todo/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [task, setTask] = useState("");
+  const [todo, setTodo] = useState([]);
+
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+
+    setTask("")
+    setTodo([Date.now(), task, false])
+    console.log(todo);
+  }
+  const handleDelete = () => {
+    setTodo(todo.filter((_, i) => i !== todo.length - 1))
+  }
+
+  const handleUpdate = () => {
+
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="max-w-3xl h-screen border-black border-2 text-center mx-auto pt-16">
+      <input className="border-2 p-2 mr-3" type="text" placeholder="Enter a task" required value={task} onChange={(e) => setTask(e.target.value)} />
+      <button className="border-2 p-2" type="submit" onClick={handleAddTodo}>Add Task</button>
 
-export default App
+      <ul>
+        {
+          todo.map((task, index) => {
+            <li key={index}> {index} - {task} <span onClick={handleDelete}>Delete</span> <span onClick={handleUpdate}>Done</span></li>
+          })
+        }
+      </ul>
+    </div >
+  );
+}
